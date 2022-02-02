8cd5e0a489885a9e341961e0b1874fd9f18ace7c
RyleeMuir <85696333+RyleeMuir@users.noreply.github.com>
2022-02-01 18:42:50 -0800

    ready to publish

--- index.js
+++ index.js
@@ -10,3 +10,46 @@ function myFunction() {
     }
 
 }
+
+function changeColor(work) {
+    var defaultColor = 'black', activeColor = 'yellow',
+        window = document.getElementsId('work');
+
+    for (var i = 0; i < allWork.length; i++) {
+        window[i].style.color = defaultColor;
+    }
+
+    work.style.color = activeColor;
+
+    return false;
+}
+
+
+
+
+// home = orange
+// all others = black
+
+// curOrange = home
+
+// curOrange = 1 // home
+// if 2, work
+// if 3
+
+/*
+if click_Home
+    if (curOrange = home){
+        do nothing
+    } else {   // you know home isn't orange
+        // check which is curOrange
+        // swap colors between home and about, for example
+        // update curOrange to home
+    }
+if click_Work
+    asdf
+if click_Contact
+    asdf
+if click_About
+    asdf
+
+*/
